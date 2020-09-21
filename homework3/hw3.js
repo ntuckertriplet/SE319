52da46cef0005106091c218024829bd998c246c3
njtucker <njtucker@iastate.edu>
2020-09-20 22:46:15 -0500

    because caps

--- homework3/hw3.js
+++ homework3/hw3.js
@@ -33,5 +33,6 @@ function palindrome(str) {
     var re = /[\W_]/g;
     var lowRegStr = str.toLowerCase().replace(re, '');
     var reverseStr = lowRegStr.split('').reverse().join('');
-    return reverseStr === lowRegStr;
+    if (reverseStr === lowRegStr) return "True";
+    return "False";
 }
